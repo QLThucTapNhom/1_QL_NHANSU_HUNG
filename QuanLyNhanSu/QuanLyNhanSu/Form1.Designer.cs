7aefc9ddd650cc974c610054290b8b4123ad8a68
= <=>
2020-03-11 17:41:25 +0700

    Upload main form

---
+++ QuanLyNhanSu/QuanLyNhanSu/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace QuanLyNhanSu
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonChucNang = new System.Windows.Forms.Button();
+            this.buttonQuanLy = new System.Windows.Forms.Button();
+            this.buttonTaiKhoan = new System.Windows.Forms.Button();
+            this.buttonDanhMuc = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panelTaiKhoan = new System.Windows.Forms.Panel();
+            this.buttonDangKy = new System.Windows.Forms.Button();
+            this.buttonDangNhap = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelQuanLy = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panelDanhMuc = new System.Windows.Forms.Panel();
+            this.buttonTTCaNhan = new System.Windows.Forms.Button();
+            this.buttonNhanSu = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panelChucNang = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.panelTaiKhoan.SuspendLayout();
+            this.panelQuanLy.SuspendLayout();
+            this.panelDanhMuc.SuspendLayout();
+            this.panelChucNang.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonChucNang
+            // 
+            this.buttonChucNang.Font = new System.Drawing.Font("UVN Da Lat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonChucNang.Location = new System.Drawing.Point(45, 230);
+            this.buttonChucNang.Name = "buttonChucNang";
+            this.buttonChucNang.Size = new System.Drawing.Size(115, 45);
+            this.buttonChucNang.TabIndex = 2;
+            this.buttonChucNang.Text = "Chức Năng";
+            this.buttonChucNang.UseVisualStyleBackColor = true;
+            this.buttonChucNang.Click += new System.EventHandler(this.buttonChucNang_Click);
+            // 
+            // buttonQuanLy
+            // 
+            this.buttonQuanLy.Font = new System.Drawing.Font("UVN Da Lat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonQuanLy.Location = new System.Drawing.Point(45, 180);
+            this.buttonQuanLy.Name = "buttonQuanLy";
+            this.buttonQuanLy.Size = new System.Drawing.Size(115, 45);
+            this.buttonQuanLy.TabIndex = 3;
+            this.buttonQuanLy.Text = "Quản Lý";
+            this.buttonQuanLy.UseVisualStyleBackColor = true;
+            this.buttonQuanLy.Click += new System.EventHandler(this.buttonQuanLy_Click);
+            // 
+            // buttonTaiKhoan
+            // 
+            this.buttonTaiKhoan.Font = new System.Drawing.Font("UVN Da Lat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTaiKhoan.Location = new System.Drawing.Point(45, 80);
+            this.buttonTaiKhoan.Name = "buttonTaiKhoan";
+            this.buttonTaiKhoan.Size = new System.Drawing.Size(115, 45);
+            this.buttonTaiKhoan.TabIndex = 0;
+            this.buttonTaiKhoan.Text = "Tài Khoản";
+            this.buttonTaiKhoan.UseVisualStyleBackColor = true;
+            this.buttonTaiKhoan.Click += new System.EventHandler(this.buttonTaiKhoan_Click);
+            // 
+            // buttonDanhMuc
+            // 
+            this.buttonDanhMuc.Font = new System.Drawing.Font("UVN Da Lat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDanhMuc.Location = new System.Drawing.Point(45, 130);
+            this.buttonDanhMuc.Name = "buttonDanhMuc";
+            this.buttonDanhMuc.Size = new System.Drawing.Size(115, 45);
+            this.buttonDanhMuc.TabIndex = 1;
+            this.buttonDanhMuc.Text = "Danh Mục";
+            this.buttonDanhMuc.UseVisualStyleBackColor = true;
+            this.buttonDanhMuc.Click += new System.EventHandler(this.buttonDanhMuc_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel3.Location = new System.Drawing.Point(206, 59);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(646, 438);
+            this.panel3.TabIndex = 2;
+            // 
+            // panelTaiKhoan
+            // 
+            this.panelTaiKhoan.Controls.Add(this.buttonDangKy);
+            this.panelTaiKhoan.Controls.Add(this.buttonDangNhap);
+            this.panelTaiKhoan.Location = new System.Drawing.Point(85, 281);
+            this.panelTaiKhoan.Name = "panelTaiKhoan";
+            this.panelTaiKhoan.Size = new System.Drawing.Size(115, 97);
+            this.panelTaiKhoan.TabIndex = 4;
+            // 
+            // buttonDangKy
+            // 
+            this.buttonDangKy.Font = new System.Drawing.Font("UVN Da Lat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDangKy.Location = new System.Drawing.Point(22, 50);
+            this.buttonDangKy.Name = "buttonDangKy";
+            this.buttonDangKy.Size = new System.Drawing.Size(72, 34);
+            this.buttonDangKy.TabIndex = 8;
+            this.buttonDangKy.Text = "Đăng Ký";
+            this.buttonDangKy.UseVisualStyleBackColor = true;
+            // 
+            // buttonDangNhap
+            // 
+            this.buttonDangNhap.Font = new System.Drawing.Font("UVN Da Lat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDangNhap.Location = new System.Drawing.Point(22, 10);
+            this.buttonDangNhap.Name = "buttonDangNhap";
+            this.buttonDangNhap.Size = new System.Drawing.Size(72, 34);
+            this.buttonDangNhap.TabIndex = 1;
+            this.buttonDangNhap.Text = "Đăng Nhập";
+            this.buttonDangNhap.UseVisualStyleBackColor = true;
+            this.buttonDangNhap.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Location = new System.Drawing.Point(-2, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(854, 62);
+            this.panel1.TabIndex = 5;
+            // 
+            // panelQuanLy
+            // 
+            this.panelQuanLy.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.panelQuanLy.Controls.Add(this.button3);
+            this.panelQuanLy.Controls.Add(this.button1);
+            this.panelQuanLy.Controls.Add(this.button2);
+            this.panelQuanLy.Location = new System.Drawing.Point(43, 300);
+            this.panelQuanLy.Name = "panelQuanLy";
+            this.panelQuanLy.Size = new System.Drawing.Size(115, 135);
+            this.panelQuanLy.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("UVN Da Lat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(22, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Phòng Ban";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("UVN Da Lat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(22, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Nhân Viên";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panelDanhMuc
+            // 
+            this.panelDanhMuc.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.panelDanhMuc.Controls.Add(this.button7);
+            this.panelDanhMuc.Controls.Add(this.buttonTTCaNhan);
+            this.panelDanhMuc.Controls.Add(this.buttonNhanSu);
+            this.panelDanhMuc.Location = new System.Drawing.Point(64, 291);
+            this.panelDanhMuc.Name = "panelDanhMuc";
+            this.panelDanhMuc.Size = new System.Drawing.Size(115, 135);
+            this.panelDanhMuc.TabIndex = 6;
+            // 
+            // buttonTTCaNhan
+            // 
+            this.buttonTTCaNhan.Font = new System.Drawing.Font("UVN Da Lat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTTCaNhan.Location = new System.Drawing.Point(22, 50);
+            this.buttonTTCaNhan.Name = "buttonTTCaNhan";
+            this.buttonTTCaNhan.Size = new System.Drawing.Size(72, 34);
+            this.buttonTTCaNhan.TabIndex = 8;
+            this.buttonTTCaNhan.Text = "TT Cá Nhân";
+            this.buttonTTCaNhan.UseVisualStyleBackColor = true;
+            // 
+            // buttonNhanSu
+            // 
+            this.buttonNhanSu.Font = new System.Drawing.Font("UVN Da Lat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNhanSu.Location = new System.Drawing.Point(22, 9);
+            this.buttonNhanSu.Name = "buttonNhanSu";
+            this.buttonNhanSu.Size = new System.Drawing.Size(72, 34);
+            this.buttonNhanSu.TabIndex = 1;
+            this.buttonNhanSu.Text = "Nhân Sự";
+            this.buttonNhanSu.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel4.Location = new System.Drawing.Point(-2, 497);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(854, 87);
+            this.panel4.TabIndex = 7;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("UVN Da Lat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(22, 90);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(72, 34);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Bậc Lương";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("UVN Da Lat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(22, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 34);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Khác";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panelChucNang
+            // 
+            this.panelChucNang.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.panelChucNang.Controls.Add(this.button4);
+            this.panelChucNang.Controls.Add(this.button8);
+            this.panelChucNang.Controls.Add(this.button9);
+            this.panelChucNang.Location = new System.Drawing.Point(22, 321);
+            this.panelChucNang.Name = "panelChucNang";
+            this.panelChucNang.Size = new System.Drawing.Size(115, 135);
+            this.panelChucNang.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("UVN Da Lat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(22, 89);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 34);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Trợ Giúp";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("UVN Da Lat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(22, 48);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(72, 34);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Báo Cáo";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("UVN Da Lat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(22, 8);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(72, 34);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "Tìm Kiếm";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ClientSize = new System.Drawing.Size(849, 532);
+            this.Controls.Add(this.panelChucNang);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panelQuanLy);
+            this.Controls.Add(this.panelDanhMuc);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelTaiKhoan);
+            this.Controls.Add(this.buttonChucNang);
+            this.Controls.Add(this.buttonQuanLy);
+            this.Controls.Add(this.buttonTaiKhoan);
+            this.Controls.Add(this.buttonDanhMuc);
+            this.Controls.Add(this.panel3);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelTaiKhoan.ResumeLayout(false);
+            this.panelQuanLy.ResumeLayout(false);
+            this.panelDanhMuc.ResumeLayout(false);
+            this.panelChucNang.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonChucNang;
+        private System.Windows.Forms.Button buttonQuanLy;
+        private System.Windows.Forms.Button buttonTaiKhoan;
+        private System.Windows.Forms.Button buttonDanhMuc;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panelTaiKhoan;
+        private System.Windows.Forms.Button buttonDangNhap;
+        private System.Windows.Forms.Button buttonDangKy;
+        private System.Windows.Forms.Panel panelDanhMuc;
+        private System.Windows.Forms.Button buttonTTCaNhan;
+        private System.Windows.Forms.Button buttonNhanSu;
+        private System.Windows.Forms.Panel panelQuanLy;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Panel panelChucNang;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ QuanLyNhanSu/QuanLyNhanSu/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhanSu
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            viTriBanDau();
+        }
+
+        int temp;
+        public void viTriBanDau()
+        {
+            buttonTaiKhoan.Location = new Point(45, 80);
+            buttonDanhMuc.Location = new Point(45, 130);
+            buttonQuanLy.Location = new Point(45, 180);
+            buttonChucNang.Location = new Point(45, 230);
+            panelTaiKhoan.Hide();
+            panelDanhMuc.Hide();
+            panelChucNang.Hide();
+            panelQuanLy.Hide();
+        }
+       
+               
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+        private void buttonTaiKhoan_Click(object sender, EventArgs e)
+        {
+            if (temp == 0)
+            {
+                panelTaiKhoan.Show();
+                panelTaiKhoan.Location = new Point(45, 125);
+                buttonDanhMuc.Location = new Point(45, 220);
+                buttonQuanLy.Location = new Point(45, 270);
+                buttonChucNang.Location = new Point(45, 320);
+                temp = 1;
+            }
+            else if (temp == 1)
+            {
+                viTriBanDau();
+                temp = 0;
+            }
+        }
+
+        private void buttonDanhMuc_Click(object sender, EventArgs e)
+        {
+            if (temp == 0)
+            {
+                panelDanhMuc.Show();
+                panelDanhMuc.Location = new Point(45, 175);
+                buttonQuanLy.Location = new Point(45, 310);
+                buttonChucNang.Location = new Point(45, 360);
+                temp = 1;
+            }
+            else if (temp == 1)
+            {
+                viTriBanDau();
+                temp = 0;
+            }
+        }
+        
+        private void buttonQuanLy_Click(object sender, EventArgs e)
+        {
+            if (temp == 0)
+            {
+                panelQuanLy.Show();
+                panelQuanLy.Location = new Point(45, 225);
+                buttonChucNang.Location = new Point(45, 360);
+                temp = 1;
+            }
+            else if (temp == 1)
+            {
+                viTriBanDau();
+                temp = 0;
+            }
+        }
+
+        private void buttonChucNang_Click(object sender, EventArgs e)
+        {
+            if (temp == 0)
+            {
+                panelChucNang.Show();
+                panelChucNang.Location = new Point(45, 275);
+                temp = 1;
+            }
+            else if (temp == 1)
+            {
+                viTriBanDau();
+                temp = 0;
+            }
+        }
+    }
+}
