f2f74e45d500e6a06fa734cefb1350744151c499
hieu culi <hieuculi34@gmail.com>
2020-04-08 23:09:37 +0700

    new file c#

---
+++ QuanLyNhanSu/QuanLyNhanSu/QuanLyNhanSu/ConnectDatabase.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace QuanLyNhanSu
+{
+    class ConnectDatabase
+    {
+        string strConnect = "Data Source=DESKTOP-4H2CDN2;Initial Catalog=TTN_QLNHANSU_01;Integrated Security=True";
+        SqlConnection sqlConnection;
+        SqlCommand sqlCommand;
+        SqlDataAdapter sqlDataAdapter;
+        SqlDataReader sqlDataReader;
+        DataSet dataSet = new DataSet();
+
+        void KetNoi()
+        {
+            sqlConnection = new SqlConnection(strConnect);
+            sqlConnection.Open();
+        }
+
+        void NgatKetNoi()
+        {
+            sqlConnection.Close();
+        }
+
+        public void ThucThiKetNoi(string strsql)
+        {
+            KetNoi();
+            sqlCommand = new SqlCommand(strsql, sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            NgatKetNoi();
+        }
+        public void loadDataGridView(DataGridView dg, string strselect)
+        {
+            dataSet.Clear();
+            sqlDataAdapter = new SqlDataAdapter(strselect, strConnect);
+            sqlDataAdapter.Fill(dataSet, "query");
+            dg.DataSource = dataSet.Tables[0];
+        }
+
+        public void loadComboBox(ComboBox cb, string strselect)
+        {
+            KetNoi();
+            sqlCommand = new SqlCommand(strselect, sqlConnection);
+            sqlDataReader = sqlCommand.ExecuteReader();
+            while (sqlDataReader.Read())
+            {
+                cb.Items.Add(sqlDataReader[0].ToString());
+            }
+            NgatKetNoi();
+        }
+        public void loadComboBox_Show(ComboBox cb, string strselect)
+        {
+            KetNoi();
+            sqlCommand = new SqlCommand(strselect, sqlConnection);
+            sqlDataReader = sqlCommand.ExecuteReader();
+            while (sqlDataReader.Read())
+            {
+                cb.Text = (sqlDataReader[0].ToString());
+            }
+            NgatKetNoi();
+        }
+        public void loadTextBox(TextBox tb, string strselect)
+        {
+            KetNoi();
+            sqlCommand = new SqlCommand(strselect, sqlConnection);
+            sqlDataReader = sqlCommand.ExecuteReader();
+
+            if (sqlDataReader.Read())
+            {
+                tb.Text = sqlDataReader[0].ToString();
+            }
+            NgatKetNoi();
+        }
+        public bool Check(string temp, string strsql)
+        {
+
+            KetNoi();
+            bool check = false;
+            sqlCommand = new SqlCommand(strsql, sqlConnection);
+            sqlDataReader = sqlCommand.ExecuteReader();
+            while (sqlDataReader.Read())
+            {
+                if (sqlDataReader[0].ToString().Trim() == temp)
+                    check = true;
+            }
+            NgatKetNoi();
+            return check;
+        }
+
+
+    }
+}
---
+++ QuanLyNhanSu/QuanLyNhanSu/QuanLyNhanSu/UI/FormTimKiem.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace QuanLyNhanSu
+{
+    partial class FormTimKiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButtonCMT = new System.Windows.Forms.RadioButton();
+            this.radioButtonHoTen = new System.Windows.Forms.RadioButton();
+            this.radioButtonMaNV = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.textBoxSearch);
+            this.groupBox1.Font = new System.Drawing.Font("Star Jedi", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(39, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(687, 129);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::QuanLyNhanSu.Properties.Resources.Icon0019;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 62);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.Location = new System.Drawing.Point(122, 56);
+            this.textBoxSearch.Multiline = true;
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(526, 30);
+            this.textBoxSearch.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButtonCMT);
+            this.groupBox2.Controls.Add(this.radioButtonHoTen);
+            this.groupBox2.Controls.Add(this.radioButtonMaNV);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.groupBox2.Location = new System.Drawing.Point(39, 147);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(687, 56);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioButtonCMT
+            // 
+            this.radioButtonCMT.AutoSize = true;
+            this.radioButtonCMT.Location = new System.Drawing.Point(555, 20);
+            this.radioButtonCMT.Name = "radioButtonCMT";
+            this.radioButtonCMT.Size = new System.Drawing.Size(64, 19);
+            this.radioButtonCMT.TabIndex = 2;
+            this.radioButtonCMT.TabStop = true;
+            this.radioButtonCMT.Text = "Địa chỉ";
+            this.radioButtonCMT.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonHoTen
+            // 
+            this.radioButtonHoTen.AutoSize = true;
+            this.radioButtonHoTen.Location = new System.Drawing.Point(358, 21);
+            this.radioButtonHoTen.Name = "radioButtonHoTen";
+            this.radioButtonHoTen.Size = new System.Drawing.Size(85, 19);
+            this.radioButtonHoTen.TabIndex = 1;
+            this.radioButtonHoTen.TabStop = true;
+            this.radioButtonHoTen.Text = "Phòng Ban";
+            this.radioButtonHoTen.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMaNV
+            // 
+            this.radioButtonMaNV.AutoSize = true;
+            this.radioButtonMaNV.Location = new System.Drawing.Point(189, 21);
+            this.radioButtonMaNV.Name = "radioButtonMaNV";
+            this.radioButtonMaNV.Size = new System.Drawing.Size(63, 19);
+            this.radioButtonMaNV.TabIndex = 0;
+            this.radioButtonMaNV.TabStop = true;
+            this.radioButtonMaNV.Text = "Mã NV";
+            this.radioButtonMaNV.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.buttonSearch);
+            this.groupBox3.Controls.Add(this.buttonExit);
+            this.groupBox3.Location = new System.Drawing.Point(39, 337);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(687, 55);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.buttonSearch.Image = global::QuanLyNhanSu.Properties.Resources.tim;
+            this.buttonSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonSearch.Location = new System.Drawing.Point(432, 14);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(70, 35);
+            this.buttonSearch.TabIndex = 7;
+            this.buttonSearch.Text = "Tìm";
+            this.buttonSearch.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.buttonExit.Image = global::QuanLyNhanSu.Properties.Resources.close1;
+            this.buttonExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonExit.Location = new System.Drawing.Point(531, 14);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(70, 35);
+            this.buttonExit.TabIndex = 6;
+            this.buttonExit.Text = "Thoát";
+            this.buttonExit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 209);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(687, 123);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // FormTimKiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 401);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(777, 440);
+            this.Name = "FormTimKiem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormTimKiem";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButtonCMT;
+        private System.Windows.Forms.RadioButton radioButtonHoTen;
+        private System.Windows.Forms.RadioButton radioButtonMaNV;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
